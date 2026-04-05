13f5f9a95168a161b51c0d1da21cc710703efdba
Aidan <39303193+aidanmastro@users.noreply.github.com>
2026-04-05 17:38:56 +1000

    java-exercises: completed 06-repeatString, 07-reverseString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(text, repeat) {
 
+    if (repeat < 0) {
+
+        return 'ERROR';
+    }
+
+    let result = ''; 
+
+    for (let i = 0; i < repeat; i++) {
+
+        result += text;
+    }
+
+    return result; 
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(text) {
 
+    const reversedArray = [];
+
+    for(let i = text.length - 1; i >= 0; i--) {
+
+        reversedArray.push(text.charAt(i));
+    }
+
+    let reversedString = reversedArray.join('');
+
+    return reversedString;
 };
+
+reverseString("peak");
 
 // Do not edit below this line
 module.exports = reverseString;
