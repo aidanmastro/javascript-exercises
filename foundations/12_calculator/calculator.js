c10f71301c40176cd1a10f09672bcb8f0ee6912a
Aidan <39303193+aidanmastro@users.noreply.github.com>
2026-04-21 00:32:43 +1000

    completed 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,60 @@
-const add = function() {
+// Variables
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+const add = function(num1, num2) {
+	return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+
+const sum = function(arr) {
+
+  let totalSum = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+    totalSum += arr[i];
+  }
+
+  return totalSum;
+};
+
+const multiply = function(arr) {
+
+  let totalProduct = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    totalProduct *= arr[i];
+  }
+
+  return totalProduct;
+
+};
+
+const power = function(base, exponent) {
 	
+  let result = 1;
+
+  for (let i = 0; i < exponent; i++) {
+    result *= base;
+  }
+
+  return result;
 };
 
-const subtract = function() {
-	
+const factorial = function(n) {
+  
+  let result = 1;
+
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
